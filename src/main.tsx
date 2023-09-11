b570010a4527f71ecc1ab256e7c67f10201ed2db
= <=>
2023-09-11 10:32:05 +0300

    Add main layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import DiaryPage from './pages/diary/index.tsx';
 
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "diary",
+        element: <DiaryPage />
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
